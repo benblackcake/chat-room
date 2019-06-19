48018e437f27611551f7d94f3aa9d5f2d6725a4f
benblackcake <ben.chang@aiesec.net>
2019-06-19 22:18:23 +0800

    new asgi.py

--- django_chat_room/routing.py
+++ django_chat_room/routing.py
@@ -2,7 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import chat.routing
-
+ASGI_APPLICATION = "django_chat_room.routing.application"
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
